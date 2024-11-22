0e13cf206a41a56365f5b5ef2de34e16bdc1e1bd
chik0di <chikodiobu@gmail.com>
2024-11-22 03:24:19 +0100

    Update Schema

---
+++ inventory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[inventory]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [product_id] NCHAR(10) NOT NULL, 
+    [quantity_added] INT NOT NULL, 
+    [quantity_removed] INT NOT NULL, 
+    [date] DATE NOT NULL, 
+    CONSTRAINT [FK_inventory_product] FOREIGN KEY ([product_id]) REFERENCES [product]([product_id])
+)
--- order_detail.sql
+++ order_detail.sql
@@ -5,7 +5,7 @@
     [product_id] NCHAR(10) NOT NULL, 
     [quantity] NCHAR(10) NOT NULL, 
     [price_per_unit] NCHAR(10) NOT NULL, 
-    [discount] NCHAR(10) NULL, 
+    [discount] REAL NULL, 
     CONSTRAINT [FK_order_detail_product] FOREIGN KEY ([product_id]) REFERENCES [product]([product_id]), 
     CONSTRAINT [FK_order_detail_order] FOREIGN KEY ([order_id]) REFERENCES [order]([order_id])
 )
