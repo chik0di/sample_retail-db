35669bdefd5abc27fd9c68526bb1a0ae61728787
chik0di <chikodiobu@gmail.com>
2024-11-25 13:42:34 +0100

    Update Order Schema

--- order.sql
+++ order.sql
@@ -4,5 +4,7 @@
     [customer_id] INT NOT NULL, 
     [order_date] DATE NOT NULL, 
     [total_amount] DECIMAL(5, 2) NOT NULL , 
-    [employee_id] INT NOT NULL
+    [employee_id] INT NOT NULL, 
+    CONSTRAINT [FK_order_customer] FOREIGN KEY ([customer_id]) REFERENCES [customer]([customer_id]), 
+    CONSTRAINT [FK_order_employee] FOREIGN KEY ([employee_id]) REFERENCES [employee]([employee_id])
 )
